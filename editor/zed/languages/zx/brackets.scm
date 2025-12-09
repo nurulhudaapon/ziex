f13374d01e7fc10e48eb016a6bf18898993f8cc1
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-12-09 22:54:43 +0600

    fix: zed brackets queries

--- editor/zed/languages/zx/brackets.scm
+++ editor/zed/languages/zx/brackets.scm
@@ -4,24 +4,3 @@
 ("\"" @open "\"" @close)
 (payload "|" @open "|" @close)
 ("'" @open "'" @close)
-
-; ZX-specific brackets
-(zx_element
-  "<" @open
-  ">" @close)
-
-(zx_element
-  "</" @open
-  ">" @close)
-
-(zx_self_closing_element
-  "<" @open
-  "/>" @close)
-
-(zx_fragment
-  "<>" @open
-  "</>" @close)
-
-(zx_expression_block
-  "{" @open
-  "}" @close)
