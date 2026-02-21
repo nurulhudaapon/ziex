ac8bccefe2d38a9141729addbc5cfedda6389c1c
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-02-21 15:19:30 +0600

    doc: update plugin docs

--- site/pages/learn/examples/esbuild_plugin.zig
+++ site/pages/learn/examples/esbuild_plugin.zig
@@ -4,12 +4,13 @@ const zx = @import("zx");
 pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{ .name = "my-app" });
 
-    try zx.init(b, exe, .{
-        .plugins = &.{
-            zx.plugins.esbuild(b, .{
-                .input = b.path("app/main.ts"),
-                .output = b.path("{outdir}/assets/main.js"),
-            }),
-        },
-    });
+    var zx_build = try zx.init(b, exe, .{});
+
+    var assetsdir = zx_build.assetsdir;
+    zx_build.addPlugin(
+        zx.plugins.esbuild(b, .{
+            .input = b.path("app/main.ts"),
+            .output = assetsdir.path(b, "main.js"),
+        }),
+    );
 }
