678537d75c811e7d1cfc86a49c02d21a74d692c2
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-02-24 01:16:43 +0600

    feat(pg): add validation for playground method

--- site/pages/playground/scripts/template/main.zig
+++ site/pages/playground/scripts/template/main.zig
@@ -6,6 +6,17 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
     var aw = std.Io.Writer.Allocating.init(allocator);
 
+    if (!@hasDecl(pg, "Playground")) {
+        @compileError("`pub fn Playground(allocator: zx.Allocator) zx.Component` is required");
+    }
+
+    const pg_type = @TypeOf(pg.Playground);
+    const expected_type = fn (allocator: zx.Allocator) zx.Component;
+
+    if (pg_type != expected_type) {
+        @compileError("`Playground` type needs to be `fn (allocator: zx.Allocator) zx.Component`");
+    }
+
     try pg.Playground(allocator).render(&aw.writer);
     try std.fs.File.stdout().writeAll(aw.written());
 }
