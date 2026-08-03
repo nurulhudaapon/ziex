1df493e08f92a954d2297dbc085e67694f6d6a94
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-08-03 20:22:55 +0600

    docs: update seed id

--- site/build.zig
+++ site/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const ziex = @import("ziex");
 
 const build_zon = @import("build.zig.zon");
-const ziex_version = 8; // Increment this when site js changes
+const ziex_version = 9; // Increment this when site js changes
 
 pub fn build(b: *std.Build) !void {
     // --- Target and Optimize from `zig build` arguments ---
